269832235cc58566f7e416f3b0ff5b968776d3ef
Josiel Delgadillo <josieldelgadillo98@gmail.com>
2021-02-14 19:14:31 -0500

    Update getStockPricePerM.py

--- SourceCode/Polygon/getStockPricePerM.py
+++ SourceCode/Polygon/getStockPricePerM.py
@@ -14,26 +14,42 @@ def main():
     getStockPrice(company)
 
 def getStockPrice(ticker:str):
+
     API_KEY = os.getenv("POLYGON_API_KEY")
 
-    url = "https://api.polygon.io/v2/aggs/ticker/"+ ticker.upper() + "/range/5/minute/2013-02-16/2013-08-16?unadjusted=true&sort=asc&limit=50000&apiKey=" + API_KEY
+    # Quartley requests
+    tslaDates = [
+        {"start":"2013-07-17","end":"2013-10-17"},
+        {"start":"2013-10-17","end":"2014-01-17"},
+        {"start":"2014-01-17","end":"2014-04-17"},
+        {"start":"2014-04-17","end":"2014-07-17"},
+        {"start":"2014-07-17","end":"2014-10-17"}
+    ]
 
-    # headers for Polygon API request
-    payload = {}
-    headers = {}
+    for dates in tslaDates:
+        print("Starting the " + dates['start'] + " batch")
 
-    # make Polygon Aggregates (Bar) API request
-    try:
-        response = requests.request("GET", url, headers = headers, data = payload)
-        db(response.text, ticker.upper())
-    except Exception as e: print(e)
-    except:
-        print(response.status_code)
-        print(response.raise_for_status)
+        # Build API URL
+        url = "https://api.polygon.io/v2/aggs/ticker/"+ ticker.upper() + "/range/5/minute/" + dates['start'] + "/" + dates['end'] + "?unadjusted=true&sort=asc&limit=50000&apiKey=" + API_KEY
+
+        # headers for Polygon API request
+        payload = {}
+        headers = {}
+
+        # make Polygon Aggregates (Bar) API request
+        try:
+            response = requests.request("GET", url, headers = headers, data = payload)
+            db(response.text, ticker.upper())
+        except Exception as e: print(e)
+        except:
+            print(response.status_code)
+            print(response.raise_for_status)
 
 def db(data:str, ticker:str):
+
     # Make database connection
     DATABASE_CRED = json.loads(os.getenv("DATABASE_CREDS"))
+    
     conn = psycopg2.connect(database=DATABASE_CRED["database"], user = DATABASE_CRED["user"], password = DATABASE_CRED["password"], host = DATABASE_CRED["host"], port = DATABASE_CRED["port"])
     print("Connected to " + conn.dsn)
     cur = conn.cursor()
@@ -71,16 +87,14 @@ def db(data:str, ticker:str):
 
     # Convert JSON text into JSON object
     res = json.loads(data)
+    print("Starting insert statements")
 
     # Loop through all items within JSON object and insert into database.schema.table
     for data in res['results']:
         timestamp = datetime.datetime.fromtimestamp((int(data['t'])/1000))
-        tickerDate = timestamp.strftime("%x")
-        tickerTime = timestamp.strftime("%X")
 
         # Create INSERT query
-        sql = '''INSERT INTO josiel_project.''' + ticker.lower() + '''(tickerdate,tickertime,open,high,low,close,volume) VALUES( ''' + "'" + tickerDate + "','" + tickerTime + "'," + str(data['o']) + "," + str(data['h']) + "," + str(data['l']) + "," + str(data['c']) + "," + str(data['v']) + ''');'''
-        print(tickerDate + " " + tickerTime)
+        sql = '''INSERT INTO josiel_project.''' + ticker.lower() + '''(tickerdate,tickertime,open,high,low,close,volume) VALUES( ''' + "'" + datetime.datetime.fromtimestamp((int(data['t'])/1000)).strftime("%x") + "','" + datetime.datetime.fromtimestamp((int(data['t'])/1000)).strftime("%X") + "'," + str(data['o']) + "," + str(data['h']) + "," + str(data['l']) + "," + str(data['c']) + "," + str(data['v']) + ''');'''
         cur.execute(sql)
         conn.commit()
 
